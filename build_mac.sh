e82f9d4c26c2438aaa8472ad25d2ea0a63b4800c
Pawan Verma <pverma@dimagi.com>
2024-03-20 12:19:29 +0530

    Mac build script

---
+++ build_mac.sh
@@ -0,0 +1,1 @@
+g++ $1 -std=c++20 `pkg-config --libs --cflags raylib` -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL /opt/homebrew/Cellar/raylib/5.0/lib/libraylib.a -o ./bin/$2 && ./bin/$2
