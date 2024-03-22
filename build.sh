41b73e466035464efc47ad5ae9ee09310dca7d1c
pxwxnvermx <pawanjbs5@gmail.com>
2024-03-22 15:30:29 +0530

    update build scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-g++ $1 -std=c++20 -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -pedantic -Wall -Wextra -o ./bin/$2 && ./bin/$2
+#! /usr/bin/bash
+#
+set -xe
+
+g++ $1 -std=c++20 -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -pedantic -Wall -Wextra -o ./bin/$2 
+./bin/$2
--- build_mac.sh
+++ build_mac.sh
@@ -1,1 +1,6 @@
-g++ $1 -std=c++20 `pkg-config --libs --cflags raylib` -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL /opt/homebrew/Cellar/raylib/5.0/lib/libraylib.a -o ./bin/$2 && ./bin/$2
+#! /usr/bin/bash
+
+set -xe
+
+g++ $1 -std=c++20 `pkg-config --libs --cflags raylib` -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL /opt/homebrew/Cellar/raylib/5.0/lib/libraylib.a -o ./bin/$2 
+./bin/$2
