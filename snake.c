4635e0cf5822c02234e602ea0b2ee8f154a96ca2
pxwxnvermx <pawanjbs5@gmail.com>
2024-04-09 19:48:26 +0530

    Added collision and food, removed vector2's

--- snake.c
+++ snake.c
@@ -1,10 +1,21 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+const int factor = 80;
+const int screenWidth = factor * 16;
+const int screenHeight = factor * 9;
+const int tile_size = 32;
+const int rows = screenWidth / tile_size;
+const int cols = screenHeight / tile_size;
+
+typedef enum { SNAKE, FOOD, AIR } TileType;
+
 typedef struct node {
-  Vector2 pos;
+  int x;
+  int y;
   struct node *prev;
   struct node *next;
 } SnakeNode;
@@ -12,13 +23,15 @@ typedef struct node {
 typedef struct {
   SnakeNode *head;
   SnakeNode *tail;
-  size_t len;
-  Vector2 dir;
+  int len;
+  int dir_x;
+  int dir_y;
 } Snake;
 
-SnakeNode *create_node(Vector2 pos) {
+SnakeNode *create_node(int x, int y) {
   SnakeNode *node = (SnakeNode *)malloc(sizeof(SnakeNode));
-  node->pos = pos;
+  node->x = x;
+  node->y = y;
   node->prev = NULL;
   node->next = NULL;
   return node;
@@ -44,79 +57,132 @@ void delete_tail(Snake *snake) {
   return;
 }
 
-void update_snake(Snake *snake) {
-  SnakeNode *cur = snake->head;
-  Vector2 vel = Vector2Multiply(snake->dir, (Vector2){1, 1});
-  Vector2 pos = Vector2Add(cur->pos, vel);
-  SnakeNode *new_head = create_node(pos);
-
-  push_front(snake, new_head);
-
-  delete_tail(snake);
-  return;
-}
-
 void draw_snake(Snake *snake) {
   SnakeNode *cur = snake->head;
   while (cur != NULL) {
-    DrawRectangle((int)cur->pos.x * 32 % GetScreenWidth(),
-                  (int)cur->pos.y * 32 % GetScreenHeight(), 32, 32, GRAY);
+    DrawRectangle(cur->x * tile_size, cur->y * tile_size, tile_size - 2,
+                  tile_size - 2, GRAY);
     cur = cur->next;
   }
   return;
 }
 
-int main() {
-  int factor = 80;
-  int screenWidth = factor * 16;
-  int screenHeight = factor * 9;
+Snake init_snake() {
+  Snake snake;
+  snake.head = create_node(1, 1);
+  snake.tail = snake.head;
+  snake.dir_x = 1;
+  snake.dir_y = 0;
+  snake.len = 0;
+  push_front(&snake, create_node(2, 1));
+  push_front(&snake, create_node(3, 1));
+  push_front(&snake, create_node(4, 1));
+  return snake;
+}
 
+void init_board(TileType board[rows][cols]) {
+  for (int x = 0; x < rows; x++) {
+    for (int y = 0; y < cols; y++) {
+      board[x][y] = AIR;
+    }
+  }
+}
+
+void update_snake(Snake *snake, TileType board[rows][cols], Vector2 *food) {
+  SnakeNode *cur = snake->head;
+  int x = cur->x + snake->dir_x;
+  int y = cur->y + snake->dir_y;
+  if (x < 0)
+    x = rows - 1;
+  if (x > rows)
+    x = 0;
+  if (y < 0)
+    y = cols - 1;
+  if (y > cols)
+    y = 0;
+  int check_snake = board[x][y] == SNAKE;
+  if (check_snake != 0) {
+    *snake = init_snake();
+    init_board(board);
+    food->x = -1;
+    food->y = -1;
+    return;
+  }
+  int check_food = board[x][y] == FOOD && x == food->x && y == food->y;
+
+  SnakeNode *new_head = create_node(x, y);
+  board[new_head->x][new_head->y] = SNAKE;
+  push_front(snake, new_head);
+  if (check_food == 0) {
+    board[snake->tail->x][snake->tail->y] = AIR;
+    delete_tail(snake);
+  } else {
+    food->x = -1;
+    food->y = -1;
+  }
+  return;
+}
+
+int main() {
+  TileType board[rows][cols];
+  init_board(board);
   InitWindow(screenWidth, screenHeight, "Snake");
   InitAudioDevice();
   SetTargetFPS(60);
-
-  Snake snake;
-  snake.head = create_node((Vector2){1, 1});
-  snake.tail = snake.head;
-  snake.dir = (Vector2){1, 0};
-  snake.len = 0;
-  push_front(&snake, create_node((Vector2){2, 1}));
-  push_front(&snake, create_node((Vector2){3, 1}));
-  push_front(&snake, create_node((Vector2){4, 1}));
-
   float frame = 0;
-  Vector2 food = {0};
+  int allow_input = 1;
+
+  Snake snake = init_snake();
+  board[1][1] = SNAKE;
+  board[2][1] = SNAKE;
+  board[3][1] = SNAKE;
+  board[4][1] = SNAKE;
+  Vector2 food = (Vector2){-1, -1};
+  char *score = malloc(25 * sizeof(char));
 
   while (!WindowShouldClose()) {
+    sprintf(score, "Score: %d", snake.len);
     const float delta = GetFrameTime();
     frame += delta;
-
-    if (IsKeyDown(KEY_W)) {
-      snake.dir.x = 0;
-      snake.dir.y = -1;
-    } else if (IsKeyDown(KEY_A)) {
-      snake.dir.x = -1;
-      snake.dir.y = 0;
-    } else if (IsKeyDown(KEY_S)) {
-      snake.dir.x = 0;
-      snake.dir.y = 1;
-    } else if (IsKeyDown(KEY_D)) {
-      snake.dir.x = 1;
-      snake.dir.y = 0;
-    } else if (IsKeyPressed(KEY_T)) {
-      delete_tail(&snake);
+    if (IsKeyDown(KEY_W) && snake.dir_y != 1 && allow_input != 0) {
+      snake.dir_x = 0;
+      snake.dir_y = -1;
+      allow_input = 0;
+    } else if (IsKeyDown(KEY_A) && snake.dir_x != 1 && allow_input != 0) {
+      snake.dir_x = -1;
+      snake.dir_y = 0;
+      allow_input = 0;
+    } else if (IsKeyDown(KEY_S) && snake.dir_y != -1 && allow_input != 0) {
+      snake.dir_x = 0;
+      snake.dir_y = 1;
+      allow_input = 0;
+    } else if (IsKeyDown(KEY_D) && snake.dir_x != -1 && allow_input != 0) {
+      snake.dir_x = 1;
+      snake.dir_y = 0;
+      allow_input = 0;
     }
 
     if (frame > 0.15f) {
-      update_snake(&snake);
+      allow_input = 1;
       frame = 0;
+      update_snake(&snake, board, &food);
+      while (food.x == -1 && GetRandomValue(0, 1) == 0) {
+        int x = GetRandomValue(0, rows - 1);
+        int y = GetRandomValue(0, cols - 1);
+        if (board[x][y] == AIR) {
+          board[x][y] = FOOD;
+          food.x = x;
+          food.y = y;
+          break;
+        }
+      }
     }
 
     BeginDrawing();
     ClearBackground(RAYWHITE);
-
     draw_snake(&snake);
-
+    DrawCircle(food.x * tile_size + 16, food.y * tile_size + 16, 10, RED);
+    DrawText(score, 8, 8, 20, BLACK);
     EndDrawing();
   }
 
@@ -126,8 +192,5 @@ int main() {
 }
 
 /*
- * TODO: Create a game board
- * TODO: Spawn Food
- * TODO: Collision
  * TODO: Sound
  * */
